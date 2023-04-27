2c02ad974c65e16ebe66c7bb654733ce749d52ea
Taha Yassine <yassinetaha1997@gmail.com>
2023-04-28 00:52:39 +0200

    Fix get_size for negative values

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def get_size(bytes, suffix="B"):
     """
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
+        if abs(bytes) < factor:
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
